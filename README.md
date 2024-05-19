251cf560f78e05b8da88c022b7c2121ad7f02cfa
saubuny <saubuny@gmail.com>
2024-05-19 18:18:07 -0400

    progress: implement brackets, hello world test passed!

--- README.md
+++ README.md
@@ -5,13 +5,14 @@ Run your esoteric programming language with an esoteric compiler! (Why would you
 ## "Features"
 
 - Compiling Brainfuck to x86 fasm assembly (this means fasm is a dependency)
-- Decent error messages ??
+- Decent error messages
+- Basic optimizations
 - Linux-only (sowwy, i'm writing assembly by hand)
 - Yeah that's pretty much it
 
 ## Installation/Usage
 
-Download the script and run it yourself :) I believe you'll need at least python 3.10
+Clone the repo and run it yourself :) I believe you'll need at least python 3.10
 
 ## Why???
 
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import subprocess
 from collections import defaultdict
-import pprint
 
 source = open("examples/hello.bf")
 result = open("out.asm", "w")
@@ -11,58 +10,56 @@ result.write("entry main\n\n")
 result.write("main:\n")
 result.write("\txor r8, r8\n")
 
-# Prefind matching parenthesis, we could throw an error here
-matches: dict[int, dict[str, int]] = defaultdict(dict)
-op_br_count = 0
-cl_br_count = 0
 src = source.read()
+
+# first pass
+matches: dict[int, int] = defaultdict(int)
+stack: list[int] = []
+
+for i in range(len(src)):
+    if src[i] == "[":
+            stack.append(i)
+    elif src[i] == "]":
+            matches[stack.pop()] = i
+print(matches)
+
 for i in range(len(src)):
     match src[i]:
+        case ">":
+            result.write("\tcall next\n")
+        case "<":
+            result.write("\tcall previous\n")
+        case "+":
+            result.write("\tcall increment\n")
+        case "-":
+            result.write("\tcall decrement\n")
+        case ".":
+            result.write("\tcall output\n")
+        case ",":
+            result.write("\tcall input\n")
+        # The jumping instructions need data to be passed
         case "[":
-            matches[op_br_count]["["] = i
-            op_br_count += 1
+            result.write("\tlea r9, [arr + r8]\n") 
+            result.write("\tmovzx r10, byte [r9]\n")
+            result.write("\ttest r10, r10\n")
+            result.write(f"\tjz cl_br{matches[i]}\n")
+            result.write(f"op_br{i}:\n") 
         case "]":
-            cl_br_count += 1
-            matches[cl_br_count]["]"] = i
-pp = pprint.PrettyPrinter(depth=4)
-pp.pprint(matches)
-
-op_br_count = 0
-cl_br_count = 0
-# for char in src:
-#     match char:
-#         case ">":
-#             result.write("\tcall next\n")
-#         case "<":
-#             result.write("\tcall previous\n")
-#         case "+":
-#             result.write("\tcall increment\n")
-#         case "-":
-#             result.write("\tcall decrement\n")
-#         case ".":
-#             result.write("\tcall output\n")
-#         case ",":
-#             result.write("\tcall input\n")
-#         # The jumping instructions need data to be passed
-#         case "[":
-#             op_br_count += 1
-#             result.write("\tlea r9, [arr + r8]\n") 
-#             result.write("\tmovzx r10, byte [r9]\n")
-#             result.write("\ttest r10, r10\n")
-#             result.write(f"\tjz cl_br{matches[op_br_count]["]"]}\n")
-#             result.write(f"op_br{matches[op_br_count]["["]}:\n") 
-#         case "]":
-#             cl_br_count += 1
-#             result.write("\tlea r9, [arr + r8]\n") 
-#             result.write("\tmovzx r10, byte [r9]\n")
-#             result.write("\ttest r10, r10\n")
-#             result.write(f"\tjnz op_br{matches[cl_br_count]["["]}\n")
-#             result.write(f"cl_br{matches[cl_br_count]["]"]}:\n") 
+            j = None
+            for key, value in matches.items():
+                if value == i:
+                    j = key
+            assert j is not None
+            result.write("\tlea r9, [arr + r8]\n") 
+            result.write("\tmovzx r10, byte [r9]\n")
+            result.write("\ttest r10, r10\n")
+            result.write(f"\tjnz op_br{j}\n")
+            result.write(f"cl_br{matches[j]}:\n") 
 
 
 # Sys exit
-result.write("\txor  rdi, rdi\n")
-result.write("\tmov  rax, 60\n")
+result.write("\txor rdi, rdi\n")
+result.write("\tmov rax, 60\n")
 result.write("\tsyscall\n\n")
 
 # > instruction
@@ -111,7 +108,7 @@ result.write("arr db arr_size dup (0)")
 source.close()
 result.close()
 
-subprocess.run(["fasm", "out.asm"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-# subprocess.run(["fasm", "out.asm"])
+# subprocess.run(["fasm", "out.asm"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+subprocess.run(["fasm", "out.asm"])
 
 subprocess.run("./out")
